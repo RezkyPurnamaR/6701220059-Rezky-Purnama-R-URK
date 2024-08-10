459ebe7941eb50f9f7bb4662a2430857264d6170
RezkyPurnamaR <rezkypurnamar@gmail.com>
2024-08-10 19:48:29 +0700

    v1.4 -- Final Add Documantation

---
+++ README.md
@@ -0,0 +1,7 @@
+#README
+
+Kalkulator 
+
+Nama : Rezky Purnama R
+NIM : 6701220059
+Cabang Yang Digunakan : Cabang Master/Main Menggunakan Logika If dan Else
--- proses.js
+++ proses.js
@@ -15,7 +15,8 @@ document.getElementById('kalkulator').addEventListener('submit', function(event)
         result = number1 / number2;
     } else {
         result = "Invalid operation";
-
+    }
+    
     // Menampilkan hasil di halaman
     document.getElementById('result').textContent = "Result: " + result;
 });
